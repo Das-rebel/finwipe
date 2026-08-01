614bfd0472c7b19ba93cf60c62277854430195ca
Subhajit Das <das-rebel@users.noreply.github.com>
2026-08-01 19:12:35 +0530

    fix: add postinstall.js to copy NBFC data to ~/.finwipe/

---
+++ postinstall.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const home = process.env.HOME;
+const destDir = path.join(home, '.finwipe', 'data');
+const destFile = path.join(destDir, 'nbfcs.yaml');
+
+try {
+  fs.mkdirSync(destDir, { recursive: true });
+  const srcFile = path.join(__dirname, 'data', 'nbfcs.yaml');
+  if (fs.existsSync(srcFile)) {
+    fs.copyFileSync(srcFile, destFile);
+    console.log('FinWipe: NBFC data initialized at ~/.finwipe/data/');
+  } else {
+    console.log('FinWipe: NBFC data not found in package, skipping');
+  }
+} catch (err) {
+  console.error('FinWipe postinstall error:', err.message);
+}
